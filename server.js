77e191b5b98a440289bc0de51310550ed865a232
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2018-08-17 13:35:07 -0300

    criado o servidor api com express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    res.status(200).send({message: 'Olá Mundo! Estou funcionando.'});
+})
+app.listen(3001, () =>{
+    console.log('API rodando na porta 3001');
+})
