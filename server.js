8b1d5452e9dd23126e68b77dbfa9aca4f9a305fb
kaiofelipejs <41922744+kaiofelipejs@users.noreply.github.com>
2018-08-17 13:41:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 app.get('/',(req,res) => {
     res.status(200).send({message: 'Olá Mundo! Estou funcionando.'});
 })
+
+app.get('/ola',(req,res) => {
+    res.status(200).send({message: 'Oi, eu sou a rota Olá.'});
+})
+
 app.listen(3001, () =>{
     console.log('API rodando na porta 3001');
 })
